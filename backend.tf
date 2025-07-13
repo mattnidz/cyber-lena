2d99680ac8e70a1431a1e194e7c71a8bdd98f766
mattnidz <nidzg1md@gmail.com>
2025-07-13 18:46:53 -0400

    init

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "Default"
-    storage_account_name = "tfstatemattpoc"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-}
